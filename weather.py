9bd10a7488754b818a8c41303d7fddc766c9adb7
mzo111 <mox99911@gmail.com>
2026-02-27 01:30:03 -0700

    weather API with error handling and file logging

---
+++ weather.py
@@ -0,0 +1,30 @@
+import requests
+
+def get_weather(city):
+    try:
+    # Step 1: convert city name to coordinates
+        geo_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
+        geo_response = requests.get(geo_url, timeout=10)
+        geo_data = geo_response.json()
+        if not geo_data.get('results'):
+            print (f"City {city} does not exist or is not supported")
+            return 
+        lat = geo_data['results'][0]['latitude']
+        lon = geo_data['results'][0]['longitude']
+        
+        # Step 2: get weather using coordinates
+        weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+        weather_response = requests.get(weather_url, timeout=10)
+        weather_data = weather_response.json()
+        
+        temp = weather_data['current_weather']['temperature']
+        print(f"Current temp in {city} is {temp}°C")
+        with open("weather_log.txt", "a") as f:
+            f.write(f"{city}:{temp}C\n")
+    except requests.exceptions.Timeout:
+        print("Request timed out. Please try again later.")
+    except Exception as e:
+        print(f"error occured: {e}")
+        
+city = input("Enter a city name: ")
+get_weather(city)
